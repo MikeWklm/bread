eaa5d5d3ca0a89cda277b8aabd09210e30d04a16
MikeWklm <mw.mikewinkelmann@gmail.com>
2023-02-05 13:08:49 +0100

    first commit

---
+++ bread/__init__.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+@dataclass
+class BreadRecipe:
+    sourdough: Optional[int] = None
+    wheat: Optional[int] = None
+    full_wheat: Optional[int] = None
+    water: Optional[int] = None
+    salt: Optional[int] = None
+    total_wheat: int = field(init=False)
+    total_water: int = field(init=False)
+    full_wheat_percentage: float = field(init=False)
+    total_weight: float = field(init=False)
+    water_bp: Optional[float] = None
+    salt_bp: Optional[float] = None
+    
+    def __post_init__(self):
+        # do some sanity checks on data
+        if not ((self.water is not None) ^ (self.water_bp is not None)):
+            raise ValueError("Either water or water_bp needs to be set.")
+        if not ((self.salt is not None) ^ (self.salt_bp is not None)):
+            raise ValueError("Either salt or salt_ba needs to be set.")
+        # get total wheat (for bp calc)
+        self.total_wheat = self.wheat + self.full_wheat + self.sourdough * .5
+        # infer bp or actual amounts
+        self.full_wheat_percentage = (self.full_wheat + self.sourdough * .5) / self.total_wheat
+        if self.salt is None:
+            self.salt = self.total_wheat * self.salt_bp - self.total_wheat
+        else:
+            self.salt_bp = self.salt / self.total_wheat
+        if self.water is None:
+            total_water = self.total_wheat * self.water_bp - self.total_wheat
+            self.water = total_water - self.sourdough * .5
+        else:
+            self.water_bp = (self.water + self.sourdough * .5) / self.total_wheat
+        self.total_water = self.sourdough * .5 + self.water
+        
+        self.total_weight = self.total_water + self.total_wheat + self.salt
---
+++ bread/app.py
@@ -0,0 +1,87 @@
+import streamlit as st
+
+from bread import BreadRecipe
+
+st.header('Bake a nice Sourdough Bread!')
+
+def get_recipe(recipe_type: str) -> None:
+    if recipe_type == "abs":
+        recipe = BreadRecipe(
+            sourdough=st.session_state.abs_sourdough,
+            wheat=st.session_state.abs_wheat,
+            full_wheat=st.session_state.abs_full_wheat,
+            water=st.session_state.abs_water,
+            salt=st.session_state.abs_salt 
+        )
+    else:
+        recipe = BreadRecipe(
+        sourdough=st.session_state.rel_sourdough,
+        wheat=st.session_state.rel_wheat,
+        full_wheat=st.session_state.rel_full_wheat,
+        water_bp=st.session_state.rel_water,
+        salt_bp=st.session_state.rel_salt 
+    )
+    
+    st.session_state.bread = recipe
+
+if 'bread' not in st.session_state:
+    st.session_state.bread = None
+    
+tab_abs, tab_rel = st.tabs(["Absolute Amounts", "Baker's Percentage"])
+with tab_abs:
+    st.text("Input your absolute amounts!")
+    with st.form("abs"):
+        w, s = st.columns(2)
+        a_sd, a_wh, a_fw = st.columns(3)
+        a_sd.number_input("Sourdough [g]", min_value=25, max_value=500,
+                                    help="Sourdough in grams. Assuming 100% hydration.",
+                                    key="abs_sourdough")
+        a_wh.number_input("Wheat [g]", min_value=0, max_value=2000,
+                                help="Wheat in grams.", key="abs_wheat")
+        a_fw.number_input("Full Wheat [g]", min_value=0, max_value=2000,
+                                        help="Full Wheat in grams.", key="abs_full_wheat")
+        w.number_input("Water [g]", min_value=100, max_value=4000,
+                               help="Water in grams.", key="abs_water")
+        s.number_input("Salt [g]", min_value=1, max_value=50,
+                              help="Salt in grams.", key="abs_salt")
+        st.form_submit_button("Get Bread! (Abs)", on_click=get_recipe, args=["abs"])
+        
+    
+with tab_rel:
+    st.text("Input your Baker's Percentage")
+    with st.form("rel"):
+        w_bp, s_bp = st.columns(2)
+        sd, wh, fw = st.columns(3)
+        sd.number_input("Sourdough [g]", min_value=25, max_value=500,
+                                    help="Sourdough in grams. Assuming 100% hydration.",
+                                    key="rel_sourdough")
+        wh.number_input("Wheat [g]", min_value=0, max_value=2000,
+                                help="Wheat in grams.", key="rel_wheat")
+        fw.number_input("Full Wheat [g]", min_value=0, max_value=2000,
+                                        help="Full Wheat in grams.", key="rel_full_wheat")
+        w_bp.slider("Water BP", min_value=1.5, max_value=2., step=.01,
+                               help="Water in Baker's Percentage.", key="rel_water")
+        s_bp.slider("Salt BP", min_value=1., max_value=1.05, step=0.005, format="%f",
+                              help="Salt in Baker's Percentage.", key="rel_salt")
+        st.form_submit_button("Get Bread (Rel)!", on_click=get_recipe, args=["rel"])
+  
+if st.session_state.bread is not None:
+    recipe = st.session_state.bread
+    st.markdown(f"""
+                # Your Bread Recipe
+                ## Total Amounts
+                
+                - Sourdough: __{recipe.sourdough:.0f} gramms__
+                - Wheat:  __{recipe.wheat:.0f} gramms__
+                - Full Wheat:  __{recipe.full_wheat:.0f} gramms__
+                - Water:  __{recipe.water:.0f} gramms__
+                - Salt:  __{recipe.salt:.1f} gramms__
+                
+                ## Relative Amounts
+                
+                - Salt: __{recipe.salt_bp:.1f} BP__
+                - Water: __{recipe.water_bp:.1f} BP__
+                - Full Wheat: __{recipe.full_wheat_percentage * 100:.1f} %__
+                
+                Total Bread Weight: __{recipe.total_weight:.0f} gramms__.
+                """)
